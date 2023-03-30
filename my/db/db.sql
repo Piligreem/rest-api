0306c46d297d0bcb175a2258c38a933cbe0c00b9
Piligreem <blablanet@protonmail.com>
2023-03-30 10:02:26 +0300

    update db.sql

--- my/db/db.sql
+++ my/db/db.sql
@@ -15,12 +15,12 @@ GRANT pg_write_all_data TO admin_ws;
 -- create tables
 
 CREATE TABLE IF NOT EXISTS subdivisions (
-  subdivision_id serial PRIMARY KEY,
+  subdivision_id SERIAL PRIMARY KEY,
   title VARCHAR ( 100 ) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS employees (
-  employee_id serial PRIMARY KEY,
+  employee_id SERIAL PRIMARY KEY,
   f_name VARCHAR ( 100 ) NOT NULL,
   s_name VARCHAR ( 100 ) NOT NULL,
   surname VARCHAR ( 100 ) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS employees (
 );
 
 CREATE TABLE IF NOT EXISTS visitors (
-  visitor_id serial PRIMARY KEY,
+  visitor_id SERIAL PRIMARY KEY,
   f_name VARCHAR ( 50 ) NOT NULL,
   s_name VARCHAR ( 50 ) NOT NULL,
   surname VARCHAR ( 50 ) NOT NULL,
@@ -44,12 +44,13 @@ CREATE TABLE IF NOT EXISTS visitors (
   -- visitor_photo BYTEA NOT NULL,
   visitor_photo BYTEA,
   group_id VARCHAR(150),
-  appointment VARCHAR(50)
+  -- appointment VARCHAR(50)
+
 );
 
 
 CREATE TABLE IF NOT EXISTS pass (
-  pass_id serial PRIMARY KEY,
+  pass_id SERIAL PRIMARY KEY,
   visit_purpose TEXT,
   date_start DATE,
   date_end DATE,
@@ -63,4 +64,31 @@ CREATE TABLE IF NOT EXISTS pass (
   FOREIGN KEY (visitor_id)
       REFERENCES visitors (visitor_id)
 );
+
+
+CREATE TABLE IF NOT EXISTS users(
+  email VARCHAR(50) PRIMARY KEY,
+  password VARCHAR(128) NOT NULL,
+  list_requests_id INT[]
+);
+
+
+CREATE TABLE IF NOT EXISTS requests(
+  req_id SERIAL PRIMARY KEY,
+  req_type VARCHAR(10) NOT NULL,
+  subdivision_id INT NOT NULL,
+  req_date DATE NOT NULL,
+  req_time TIME NOT NULL,
+  req_status VARCHAR(20) NOT NULL,
+  req_status_description TEXT,
+  FOREIGN KEY (subdivision_id)
+      REFERENCES subdivisions (subdivision_id),
+  user_id VARCHAR(50) NOT NULL,
+  FOREIGN KEY (user_id)
+      REFERENCES users (email)
+);
+
+
+
+
 
